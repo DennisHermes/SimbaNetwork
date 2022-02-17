bd5c4094608d66b1042b30f9cdc2baf8a273efff
Dennis Hermes <78319503+DennisHermes@users.noreply.github.com>
2022-02-17 17:13:18 +0100

    * Changed join particle and added anti build

--- SimbaMainHub/src/me/goodgamer123/SimbaMainHub/MainClass.java
+++ SimbaMainHub/src/me/goodgamer123/SimbaMainHub/MainClass.java
@@ -1,19 +1,23 @@
 package me.goodgamer123.SimbaMainHub;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Effect;
 import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.util.Vector;
 
 public class MainClass extends JavaPlugin implements Listener {
 
 	@Override
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(this, this);
+		getServer().getPluginManager().registerEvents(new DisableBuilding(), this);
 	}
 	
 	@EventHandler
@@ -22,60 +26,33 @@ public class MainClass extends JavaPlugin implements Listener {
 		new BukkitRunnable() { 
 			@Override
 			public void run() {
-		    	Vector viewDir = e.getPlayer().getLocation().getDirection().normalize();
 				e.getPlayer().playSound(e.getPlayer().getLocation(), Sound.ENDERDRAGON_GROWL, 1, 1);
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.WITCH_MAGIC, null);
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.FIREWORKS_SPARK, null);
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.FLAME, null);
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.MAGIC_CRIT, null);
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.WITCH_MAGIC, null);
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.FIREWORKS_SPARK, null);
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.FLAME, null);
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.MAGIC_CRIT, null);
-			}
-		}.runTaskLater(MainClass.getPlugin(MainClass.class), 5);
-		new BukkitRunnable() { 
-			@Override
-			public void run() {
-		    	Vector viewDir = e.getPlayer().getLocation().getDirection().normalize();
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.WITCH_MAGIC, null);
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.FIREWORKS_SPARK, null);
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.FLAME, null);
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.MAGIC_CRIT, null);
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.WITCH_MAGIC, null);
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.FIREWORKS_SPARK, null);
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.FLAME, null);
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.MAGIC_CRIT, null);
+				e.getPlayer().getWorld().spigot().playEffect(e.getPlayer().getLocation().add(0, 1, 0), Effect.WITCH_MAGIC, 0, 0, 0, 0, 0, 1, 10, 1);
+				e.getPlayer().getWorld().spigot().playEffect(e.getPlayer().getLocation().add(0, 1, 0), Effect.FIREWORKS_SPARK, 0, 0, 0, 0, 0, 1, 10, 1);
+				e.getPlayer().getWorld().spigot().playEffect(e.getPlayer().getLocation().add(0, 1, 0), Effect.FLAME, 0, 0, 0, 0, 0, 1, 10, 1);
+				e.getPlayer().getWorld().spigot().playEffect(e.getPlayer().getLocation().add(0, 1, 0), Effect.MAGIC_CRIT, 0, 0, 0, 0, 0, 1, 10, 1);
 			}
 		}.runTaskLater(MainClass.getPlugin(MainClass.class), 10);
-		new BukkitRunnable() { 
-			@Override
-			public void run() {
-		    	Vector viewDir = e.getPlayer().getLocation().getDirection().normalize();
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.WITCH_MAGIC, null);
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.FIREWORKS_SPARK, null);
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.FLAME, null);
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.MAGIC_CRIT, null);
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.WITCH_MAGIC, null);
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.FIREWORKS_SPARK, null);
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.FLAME, null);
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.MAGIC_CRIT, null);
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (!(sender instanceof Player)) {
+			sender.sendMessage("You need to be a player to do this!");
+			return false;
+		}
+		
+		Player p = (Player) sender;
+		if (cmd.getName().equalsIgnoreCase("togglebuilding")) {
+			if (!DisableBuilding.building.contains(p)) {
+				DisableBuilding.building.add(p);
+				p.sendMessage(ChatColor.GREEN + "Buidling is now disabled!");
+			} else {
+				DisableBuilding.building.remove(p);
+				p.sendMessage(ChatColor.GREEN + "Buidling is now enabled!");
 			}
-		}.runTaskLater(MainClass.getPlugin(MainClass.class), 15);
-		new BukkitRunnable() { 
-			@Override
-			public void run() {
-		    	Vector viewDir = e.getPlayer().getLocation().getDirection().normalize();
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.WITCH_MAGIC, null);
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.FIREWORKS_SPARK, null);
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.FLAME, null);
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.MAGIC_CRIT, null);
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.WITCH_MAGIC, null);
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.FIREWORKS_SPARK, null);
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.FLAME, null);
-		    	e.getPlayer().playEffect(e.getPlayer().getLocation().clone().add(viewDir.clone().multiply(1D)), Effect.MAGIC_CRIT, null);
-			}
-		}.runTaskLater(MainClass.getPlugin(MainClass.class), 20);
+		}
+		
+		return false;
 	}
 }
 
