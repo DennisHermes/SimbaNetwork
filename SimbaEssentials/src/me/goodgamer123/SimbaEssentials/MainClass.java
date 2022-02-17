0ee322e839469afd734ca8c92de1f040bd53f6af
Dennis Hermes <78319503+DennisHermes@users.noreply.github.com>
2022-02-17 20:22:02 +0100

    * Added /op joke

---
+++ SimbaEssentials/src/me/goodgamer123/SimbaEssentials/MainClass.java
@@ -0,0 +1,58 @@
+package me.goodgamer123.SimbaEssentials;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class MainClass extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+		FileConfiguration config = this.getConfig();
+		config.addDefault("IpWhitelist", false);
+		config.options().copyDefaults(true);
+        saveConfig();
+        
+	}
+	
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		if (cmd.getName().equalsIgnoreCase("op")) {
+			if (args.length >= 1) {
+				if (sender instanceof Player) {
+					if (((Player) sender).isOp()) {
+						Bukkit.getPlayer(args[0]).setOp(true);
+						sender.sendMessage(ChatColor.GRAY + "[Server: Opped " + args[0] + "]");
+					} else {
+						sender.sendMessage(ChatColor.GRAY + "[Server: Opped " + args[0] + "]");
+						new BukkitRunnable() { 
+							@Override
+							public void run() {
+								sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6SIKE!"));
+							}
+						}.runTaskLater(MainClass.getPlugin(MainClass.class), 200);
+					}
+				} else if (sender instanceof ConsoleCommandSender) {
+					Bukkit.getPlayer(args[0]).setOp(true);
+					sender.sendMessage(ChatColor.GRAY + "[Server: Opped " + args[0] + "]");
+				}
+			} else {
+				sender.sendMessage(ChatColor.RED + "Usage: /op <player>");
+			}
+		}
+		
+		return false;
+	}
+}
+
+
