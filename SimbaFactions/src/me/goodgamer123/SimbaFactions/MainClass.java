587b19b6c22469a7dd8241773a64bf655ef71035
Dennis Hermes <78319503+DennisHermes@users.noreply.github.com>
2022-02-17 19:47:40 +0100

    * Added auto egg change

--- SimbaFactions/src/me/goodgamer123/SimbaFactions/MainClass.java
+++ SimbaFactions/src/me/goodgamer123/SimbaFactions/MainClass.java
@@ -1,11 +1,16 @@
 package me.goodgamer123.SimbaFactions;
 
+import java.util.ArrayList;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Effect;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MainClass extends JavaPlugin {
@@ -36,6 +41,25 @@ public class MainClass extends JavaPlugin {
 						p.getWorld().spigot().playEffect(p.getLocation().add(-0.5, 1, 0), Effect.COLOURED_DUST, 0, 1, 255, 0, 0, 1, 0, 64);
 						p.getWorld().spigot().playEffect(p.getLocation().add(-1, 1, 0.5), Effect.COLOURED_DUST, 0, 1, 255, 0, 0, 1, 0, 64);
 						p.getWorld().spigot().playEffect(p.getLocation().add(0.5, 2, -1), Effect.COLOURED_DUST, 0, 1, 255, 0, 0, 1, 0, 64);
+					}
+					
+					for (int slot = 0; slot < 27; slot++) {
+						if (p.getInventory().getItem(slot) != null) {
+						    if (p.getInventory().getItem(slot).getType().equals(Material.EGG) && p.getInventory().getItem(slot).getItemMeta().getLore() == null) {
+						    	int amount = 1;
+						    	ItemStack egg = new ItemStack(Material.EGG);
+						    	ItemMeta eggMeta = egg.getItemMeta();
+						    	eggMeta.setDisplayName(ChatColor.translateAlternateColorCodes('&', "&8[&e&lEksplozivna Jaja&8]"));
+						    	ArrayList<String> eggLore = new ArrayList<>();
+						        eggLore.add(ChatColor.translateAlternateColorCodes('&', "&bKAA BOOOM xD"));
+						        eggLore.add(ChatColor.translateAlternateColorCodes('&', "&bOvo nije obicno jaje xD"));
+						        eggLore.add(ChatColor.translateAlternateColorCodes('&', "&bCuvaj ga nikad ne znas kad ce ti zatrebati xD"));
+						    	eggMeta.setLore(eggLore);
+						    	egg.setItemMeta(eggMeta);
+						    	egg.setAmount(amount);
+						    	p.getInventory().setItem(slot, egg);
+						    }
+						}
 					}
 				}
 			}
